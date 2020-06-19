bd022318c1829a0f908a6a82d37bfe3030cfee32
Gilvan Carlos Teixeira Junior <49037876+juniorteixeira1805@users.noreply.github.com>
2020-06-19 12:10:31 -0300

    Consetado
    Variavel

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ require("./config/auth")(passport)
 //outros
     //escutando tempo para enviar email
     setInterval(function(){
-        let email = ["juniorteixeira1805@gmail.com","klaria6@gmail.com","kassia.milenateixeira@gmail.com","carolsilva58022@gmail.com","lidineide_nunes@hotmail.com","dayannetargino74@gmail.com","janeirode97@gmail.com","sayoolima@gmail.com","diegooliveira.msi@gmail.com","aliicecs@ufrn.edu.br","leticiacruz606@gmail.com","herlanecandido_@hotmail.com","vitor3souza@gmail.com","loiolamateus7@gmail.com","helloysaregiane@gmail.com","anaa.lusantos@gmail.com","adelsonunes.ribeiro@hotmail.com","marcosadriano740@gmail.com"]        var data = new Date()
+        let email = ["juniorteixeira1805@gmail.com","klaria6@gmail.com","kassia.milenateixeira@gmail.com","carolsilva58022@gmail.com","lidineide_nunes@hotmail.com","dayannetargino74@gmail.com","janeirode97@gmail.com","sayoolima@gmail.com","diegooliveira.msi@gmail.com","aliicecs@ufrn.edu.br","leticiacruz606@gmail.com","herlanecandido_@hotmail.com","vitor3souza@gmail.com","loiolamateus7@gmail.com","helloysaregiane@gmail.com","anaa.lusantos@gmail.com","adelsonunes.ribeiro@hotmail.com","marcosadriano740@gmail.com"]
         console.log("Verificando hora de mandar email: " + data.getHours())
         if(data.getHours() == "16"){
         Func.enviarEmail(email, email.length)
