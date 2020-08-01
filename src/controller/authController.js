1a722ce53cd3456cd6c2562dfd36e2d91e89dbf0
Mateus0808 <loiolamateus7@gmail.com>
2020-08-01 01:20:14 -0300

    Atualização do upload fotos

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -18,7 +18,8 @@
 
     const storage = multer.diskStorage({
         destination: function(req, file, cb) {
-            cb(null, "./public/uploads/")
+            cb(null, path.join(__dirname, '../public/uploads/'));
+            //cb(null, "./public/uploads/")
         },
 
         filename: function(req, file, cb) {
